15a6869945c10c3b25c9651a3f92a459f5e591db
Sergio Correia <sergio@larces.uece.br>
2010-04-30 13:42:06 -0300

    adding <stdio.h> to the list of include files and reorganizing the list ]

--- webserver.c
+++ webserver.c
@@ -1,9 +1,10 @@
-#include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <sys/types.h>
 #include <sys/wait.h>
 #include <string.h>
 #include <stdlib.h>
+#include <stdio.h>
 
 void handle(int sk)
 {
